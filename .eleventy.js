dece077621688a7139932f19bf1efcec22d8ee89
Vladimir Ilimurzin <ilimurzin@ya.ru>
2024-06-09 22:12:25 +0300

    Рестайлинг

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,9 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(EleventyHtmlBasePlugin)
 
   eleventyConfig.addPassthroughCopy('img')
+  eleventyConfig.addPassthroughCopy('main.css')
+
+  eleventyConfig.ignores.add('README.md')
 
   return {
     pathPrefix: process.env.PATHPREFIX ?? '/',
