4eb009125e8382708be16a1910df0d209634f0d3
Vladimir Ilimurzin <ilimurzin@ya.ru>
2024-06-09 20:47:11 +0300

    Удалил позаимствованные стили

--- .eleventy.js
+++ .eleventy.js
@@ -3,8 +3,6 @@ const { EleventyHtmlBasePlugin } = require('@11ty/eleventy')
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(EleventyHtmlBasePlugin)
 
-  eleventyConfig.addPassthroughCopy('./mashademianovastyles.css')
-  eleventyConfig.addPassthroughCopy('./mashademianova.js')
   eleventyConfig.addPassthroughCopy('img')
 
   return {
